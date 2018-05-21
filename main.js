1a042952119a0952438b3bc92f605dcc52ad207f
Teet Triisa <ttriisa@tlu.ee>
2018-05-21 14:17:27 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function loadHTML (myDivId, url) {
+  var xmlhttp
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest()
+  } else {
+    xmlhttp = new ActiveXObject('Microsoft.XMLHTTP')
+  }
+
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+      if (xmlhttp.status === 200) {
+        document.getElementById(myDivId).innerHTML = xmlhttp.responseText
+        var allScripts = document.getElementById(myDivId).getElementsByTagName('script')
+        for (var n = 0; n < allScripts.length; n++) {
+          eval(allScripts[n].innerHTML)// run script inside div generally not a good idea but these scripts are anyways intended to be executed.
+        }
+      } else {
+        alert('Error')
+      }
+    }
+  }
+
+  xmlhttp.open('GET', url, true)
+  xmlhttp.send()
+}
+
+window.onload = function () {
+  let button = document.getElementById('loginButton')
+  button.addEventListener('click', function (event) {
+    loadHTML('content', 'views/joinedScreen.html')
+  })
+}
